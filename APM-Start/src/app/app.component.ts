dd01920bcb3c2a56e2db391d33f440c381491c09
Ross Mawdsley <rossmawd@gmail.com>
2020-02-20 15:09:02 +0000

    adding bootstrap styling

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,3 +1,15 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'pm-root',
+  template:`
+  <div><h1>{{pageTitle}}</h1>
+  <div>My first Component</div>
+  </div>`
+})
+
 export class AppComponent {
   pageTitle: string = 'My first Title';
 
