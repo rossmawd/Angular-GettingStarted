9b07b1fc9ac9e26f2f943d516a7968a6ee7434fe
Ross Mawdsley <rossmawd@gmail.com>
2020-03-10 16:43:55 +0000

    added two way binding

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -6,6 +6,11 @@ import {Component} from '@angular/core';
 })
 export class ProductListComponent {
   pageTitle: string = 'Poop Sticks' ;
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  listFilter: string = 'cart';
+
   products: any[] = [
     {
       "productId": 1,
@@ -57,5 +62,9 @@ export class ProductListComponent {
       "starRating": 4.6,
       "imageUrl": "assets/images/xbox-controller.png"
     }
-  ]
+  ];
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
 }
