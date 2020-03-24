8997e367449c40b51fad506bef1bfe0a03de7cb2
rossmawdsley <ross.mawdsley@newday.co.uk>
2020-03-24 14:02:25 +0000

    product module implemented

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,46 +1,32 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
-
 import { AppComponent } from "./app.component";
-import { ProductListComponent } from "./products/product-list.component";
-import { FormsModule } from "@angular/forms";
-import { ConvertToSpacesPipe } from "./shared/convert-to-spaces.pipe";
-import { StarComponent } from "./shared/star.component";
-import { fromEventPattern } from "rxjs";
+
 // the Angular cli automatically imports this stuff for you
-import { ProductDetailComponent } from "./products/product-detail.component";
 import { WelcomeComponent } from "./home/welcome.component";
 import { RouterModule } from "@angular/router";
 import { ImagesComponent } from "./ross/images/images.component";
-import { ProductDetailGuard } from "./products/product-detail.guard";
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
     WelcomeComponent,
     ImagesComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+
     HttpClientModule,
     RouterModule.forRoot([
-      { path: "products", component: ProductListComponent },
-      {
-        path: "products/:id",
-        component: ProductDetailComponent,
-        canActivate: [ProductDetailGuard] //route must pass this guard (retuns T/F)
-      },
+  
       { path: "welcome", component: WelcomeComponent },
       { path: "images", component: ImagesComponent },
       { path: "", redirectTo: "welcome", pathMatch: "full" },
       { path: "**", redirectTo: "welcome", pathMatch: "full" }
-    ])
+    ]),
+    ProductModule
   ],
   bootstrap: [AppComponent]
 })
